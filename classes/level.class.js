90b91c83c2740a2e07866cfd62a9852c5ec1468b
Johannes Wächter <mail@kreuzueber.eu>
2025-10-13 14:51:01 +0200

    create new folder:level and level1.js

--- classes/level.class.js
+++ classes/level.class.js
@@ -1,5 +1,14 @@
 class level {
 
 
-    
+    enemies;
+    clouds;
+    backgroundObjects;
+
+    constructor(enemies, clouds, backgroundObjects){
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.backgroundObjects = backgroundObjects; 
+
+    }
 }
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,26 +1,9 @@
 class World {
 
     character = new Character();
-    clouds = [
-        new clouds('img/5_background/layers/4_clouds/1.png'),
-        new clouds('img/5_background/layers/4_clouds/2.png'),
-
-    ];
-    enemies = [
-        new chicken(),
-        new chicken(),
-        new chicken(),
-    ];
-    backgroundObjects = [
-        new backgroundObject('img/5_background/layers/air.png',0),
-        new backgroundObject('img/5_background/layers/3_third_layer/1.png',0),
-        new backgroundObject('img/5_background/layers/2_second_layer/1.png',0),
-        new backgroundObject('img/5_background/layers/1_first_layer/1.png',0),
-         new backgroundObject('img/5_background/layers/air.png',720),
-        new backgroundObject('img/5_background/layers/3_third_layer/2.png',720),
-        new backgroundObject('img/5_background/layers/2_second_layer/2.png',720),
-        new backgroundObject('img/5_background/layers/1_first_layer/2.png',720),
-    ];
+    clouds = level1.clouds;
+    enemies = level1.enemies;
+    backgroundObjects = level1.backgroundObjects;
     canvas;
     ctx;
     keyboard;
